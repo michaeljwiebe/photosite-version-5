e925d56722101b1b0f59bfd0326fade9509dd3ad
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-06-04 09:52:47 -0400

    seperated js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,71 @@
-// var carousel = document.getElementsByClassName("carousel")[0];
-// var photo1 = document.getElementsByClassName("photo1")[0];
-// var photo2 = document.getElementsByClassName("photo2")[0];
-// var photo3 = document.getElementsByClassName("photo3")[0];
-// var photo4 = document.getElementsByClassName("photo4")[0];
-// var photo5 = document.getElementsByClassName("photo5")[0];
 var photo6 = document.getElementsByClassName("photo6")[0];
 var photo7 = document.getElementsByClassName("photo7")[0];
 var photo8 = document.getElementsByClassName("photo8")[0];
 var photo9 = document.getElementsByClassName("photo9")[0];
-// var photos = document.getElementsByClassName("photo");
-// var thumbnails = document.getElementsByClassName("thumbnails");
 
 var hiddenPhotosArray = [];
 
 hiddenPhotosArray.push(photo6, photo7, photo8, photo9);
 
 document.addEventListener("keydown", function(event) {
-    //Add function here?
-    if (event.keyCode === 39 || event.keyCode === 37) {
-        var left = document.getElementsByClassName("left")[0];
-        var farLeft = document.getElementsByClassName("farLeft")[0];
-        var center = document.getElementsByClassName("center")[0];
-        var right = document.getElementsByClassName("right")[0];
-        var farRight = document.getElementsByClassName("farRight")[0];
-        center.classList.remove("center");
-        left.classList.remove("left");
-        right.classList.remove("right");
-        farRight.classList.remove("farRight", "skinny");
-        farLeft.classList.remove("farLeft", "skinny");
-    }
-
     if (event.keyCode === 39) {
-        farRight.classList.add("right");
-        center.classList.add("left");
-        right.classList.add("center");
-        left.classList.add("farLeft", "skinny");
-        farLeft.classList.add("hidden");
-        hiddenPhotosArray.push(farLeft);
-        var newPhoto = hiddenPhotosArray.splice(0, 1)[0];
-        newPhoto.classList.remove("hidden");
-        newPhoto.classList.add("farRight", "skinny");
+        movePhotosLeft();
     } else if (event.keyCode === 37) {
-        farLeft.classList.add("left");
-        center.classList.add("right");
-        left.classList.add("center");
-        right.classList.add("farRight", "skinny");
-        farRight.classList.add("hidden");
-        hiddenPhotosArray.splice(0, 0, farRight);
-        var newPhoto = hiddenPhotosArray.splice(
-            hiddenPhotosArray.length - 1,
-            1
-        )[0];
-        newPhoto.classList.remove("hidden");
-        newPhoto.classList.add("farLeft", "skinny");
+        movePhotosRight();
     }
+    console.log(hiddenPhotosArray);
 });
+
+function movePhotosLeft() {
+    //define photos on deck with position variables
+    var farLeft = document.getElementsByClassName("farLeft")[0];
+    var left = document.getElementsByClassName("left")[0];
+    var center = document.getElementsByClassName("center")[0];
+    var right = document.getElementsByClassName("right")[0];
+    var farRight = document.getElementsByClassName("farRight")[0];
+    //remove classes indicating position
+    farLeft.classList.remove("farLeft", "skinny");
+    left.classList.remove("left");
+    center.classList.remove("center");
+    right.classList.remove("right");
+    farRight.classList.remove("farRight", "skinny");
+    //add new position classes
+    farRight.classList.add("right");
+    right.classList.add("center");
+    center.classList.add("left");
+    left.classList.add("farLeft", "skinny");
+    //hide one photo in prep for removal then remove
+    farLeft.classList.add("hidden");
+    hiddenPhotosArray.push(farLeft);
+    //bring new photo into the mix
+    var newPhoto = hiddenPhotosArray.splice(0, 1)[0];
+    newPhoto.classList.remove("hidden");
+    newPhoto.classList.add("farRight", "skinny");
+}
+
+function movePhotosRight() {
+    //define photos on deck with position variables
+    var left = document.getElementsByClassName("left")[0];
+    var farLeft = document.getElementsByClassName("farLeft")[0];
+    var center = document.getElementsByClassName("center")[0];
+    var right = document.getElementsByClassName("right")[0];
+    var farRight = document.getElementsByClassName("farRight")[0];
+    //remove classes indicating position
+    center.classList.remove("center");
+    left.classList.remove("left");
+    right.classList.remove("right");
+    farRight.classList.remove("farRight", "skinny");
+    farLeft.classList.remove("farLeft", "skinny");
+    //add new position classes
+    farLeft.classList.add("left");
+    center.classList.add("right");
+    left.classList.add("center");
+    right.classList.add("farRight", "skinny");
+    //hide one photo in prep for removal then remove
+    farRight.classList.add("hidden");
+    hiddenPhotosArray.splice(0, 0, farRight);
+    //bring new photo into the mix
+    var newPhoto = hiddenPhotosArray.splice(hiddenPhotosArray.length - 1, 1)[0];
+    newPhoto.classList.remove("hidden");
+    newPhoto.classList.add("farLeft", "skinny");
+}
